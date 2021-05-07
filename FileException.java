4bca212cab09682cf352f755f9e53b18636a2791
Partho25-creator <acharyapartho@gmail.com>
2021-05-07 19:57:21 +0530

    java phase 1 assessment

---
+++ FileException.java
@@ -0,0 +1,20 @@
+package fileStorageSystem;
+
+public class FileException extends Exception {
+	public static void main(String args[]){
+		
+	}
+		
+
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			public FileException(String message){
+
+				super(message);
+
+			}
+
+		}
